3a11fcd17a967e67af355e8401e88ddc0b0003d1
Nate Harrell <nateharrell@users.campus>
2020-02-24 17:06:38 -0600

    Commit

---
+++ ShoppingList/AddItemControl.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ShoppingList
+{
+    /// <summary>
+    /// Interaction logic for AddItemControl.xaml
+    /// </summary>
+    public partial class AddItemControl : UserControl
+    {
+        public AddItemControl()
+        {
+            InitializeComponent();
+            AddItemButton.Click += OnAddItemButtonClick; // hooking up method to button
+        }
+
+        protected void OnAddItemButtonClick(object sender, RoutedEventArgs args)
+        {
+            /*
+            var item = ItemToAdd.Text;
+            var data = (ShoppingListData)DataContext; // throws an exception if it can't be done
+            var data2 = DataContext as ShoppingListData; // sets data2 to null if it can't be done
+            if(data2 != null)
+            {
+                // safe to work with data2
+            }
+            */
+
+            // same as above:
+            if(DataContext is ShoppingListData data) // checks if it's possible to cast. guarantees it exists and will be of type 'shoppinglistdata
+            {
+                // if true, data3 is accessible to us and is of type 'data3'.
+                data.Add(ItemToAdd.Text);
+                ItemToAdd.Text = "";
+            }
+        }
+    }
+}
---
+++ ShoppingList/ShoppingListData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace ShoppingList
+{
+    public class ShoppingListData : INotifyPropertyChanged
+    {
+        List<string> items = new List<string>();
+
+        public IEnumerable<string> Items => items.ToArray(); // public property that accesses private backing variable. .ToArray() copies the list to an array
+
+        public event PropertyChangedEventHandler PropertyChanged; // tracks if a property has been changed
+
+        /// <summary>
+        /// This means that we have to access this ShoppingList class before we can add or remove data.
+        /// </summary>
+        /// <param name="item"></param>
+        public void Add(string item)
+        {
+            items.Add(item);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Items")); // 'this' says what object we're invoking on
+        }
+        public void Remove(string item)
+        {
+            items.Remove(item);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Items")); // 'this' says what object we're invoking on
+
+        }
+    }
+}
